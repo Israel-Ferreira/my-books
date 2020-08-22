826a0e2cf7b7c09408043c7ab6a93096a74ebf3d
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2020-08-22 01:42:05 -0300

    Customizando a tela de login e de registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
+
+
+  private
+
+  def layout_by_resource
+    devise_controller? ? "#{resource_class.to_s.downcase}_devise" : 'application'
+  end
 end
